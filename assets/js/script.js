6afd40739a6337bdf47f2ea2c64ddd009146bf3d
Ivan Tepes <Ivan Tepes@DESKTOP-CKMILME>
2022-11-20 00:44:14 +0000

    Try to fix android show keyboard bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const scoreInput = document.querySelector('.footer__max-score--input');
+const wrapper = document.querySelector('.wrapper');
 console.log(scoreInput);
 
 scoreInput.addEventListener('focusin', focusInFunction);
 scoreInput.addEventListener('focusout', focusOutFunction);
 
 function focusInFunction() {
-    // document.body.style.position = 'absolute';
-    document.body.style.height = '100vh';
+    wrapper.style.height = '120%';
+}
+function focusOutFunction() {
+    wrapper.style.height = '';
 }
