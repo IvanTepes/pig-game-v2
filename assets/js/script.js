5666e2c23f79ad8f26a2dbdf99989ea2cec7e562
Ivan Tepes <Ivan Tepes@DESKTOP-CKMILME>
2022-11-20 00:02:09 +0000

    Added event listener to max score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const scoreInput = document.querySelector('.footer__max-score--input');
+console.log(scoreInput);
+
+scoreInput.addEventListener('focusin', focusInFunction);
+scoreInput.addEventListener('focusout', focusOutFunction);
+
+function focusInFunction() {
+    document.body.style.position = 'absolute';
+    document.body.style.width = '100%';
+}
