dc1c48b25ef711b5f797718336a436ee5e120949
Ivan Tepes <Ivan Tepes@DESKTOP-CKMILME>
2022-11-21 00:45:40 +0000

    Virtual keyboard fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,19 @@
 
 const scoreInput = document.querySelector('.footer__max-score--input');
 const wrapper = document.querySelector('.wrapper');
+
 console.log(scoreInput);
 
 scoreInput.addEventListener('focusin', focusInFunction);
 scoreInput.addEventListener('focusout', focusOutFunction);
 
 function focusInFunction() {
-    wrapper.style.height = '120%';
+    wrapper.style.height = 'auto';
+    document.documentElement.style.height = 'auto';
+    document.body.style.height = 'auto';
 }
 function focusOutFunction() {
     wrapper.style.height = '';
+    document.documentElement.style.height = '';
+    document.body.style.height = '';
 }
